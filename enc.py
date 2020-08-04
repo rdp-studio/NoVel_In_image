5895b66117962a80aeaa6ee6b9e4a662a1d23d6b
rdp-studio <67402215+rdp-studio@users.noreply.github.com>
2020-08-04 16:13:14 +0800

    Update enc.py

--- enc.py
+++ enc.py
@@ -3,7 +3,7 @@
 from PIL import Image
 import math
 
-def encode(text):
+def encode(text, color):
     str_len = len(text)
     width = math.ceil(str_len**0.5)
     im = Image.new("RGB", (width, width), 0x0)
@@ -11,7 +11,7 @@ def encode(text):
     x, y = 0, 0
     for i in text:
         index = ord(i)
-        rgb = ( 0,  (index & 0xFF00) >> 8,  index & 0xFF)
+        rgb = ( int(color),  (index & 0xFF00) >> 8,  index & 0xFF)
         im.putpixel( (x, y),  rgb )
         if x == width - 1:
             x = 0
@@ -20,11 +20,11 @@ def encode(text):
             x += 1
     return im
 
-def main(filename):
+def main(filename, color):
     with open(filename, encoding="utf-8") as f:
         all_text = f.read()
         
-    im = encode(all_text)
+    im = encode(all_text, color)
     im.save("{}_layout.bmp".format('.'.join(filename.split('.')[:-1])))
 
 if __name__ == '__main__':
